5e987867b440d8c055f87f322fd177393293bc39
coco7 <sergio.lopez@alumnos.uva.es>
2018-04-18 18:30:51 +0200

    Integration test for project controller finished
    ITERATION FINISHED

--- EasyRMT/src/test/java/com/Sergio/EasyRMT/IntegrationTests/ProjectControllerITest.java
+++ EasyRMT/src/test/java/com/Sergio/EasyRMT/IntegrationTests/ProjectControllerITest.java
@@ -1,10 +1,11 @@
 package com.Sergio.EasyRMT.IntegrationTests;
 
-import com.Sergio.EasyRMT.Domain.ProjectDom;
-import com.Sergio.EasyRMT.Domain.RequirementTypeDom;
-import com.Sergio.EasyRMT.Model.types.ProjectType;
+import com.Sergio.EasyRMT.Model.Project;
+import com.Sergio.EasyRMT.Repository.ProjectRepository;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +16,34 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectControllerITest {
     @Autowired
     private WebApplicationContext wac;
-
+    @Autowired
+    ProjectRepository projectRepository;
     private MockMvc mockMvc;
 
     @Before
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @AfterAll
+    public void rollback(){
+        List<Project> projectList = projectRepository.findAll();
+        int size = projectList.size();
+        Project project = projectList.get(size-1);
+        int id = project.getIdProject();
+        projectRepository.deleteProjectByIdProject(id);
     }
 
     @Test
@@ -46,29 +54,22 @@ public class ProjectControllerITest {
                 .andExpect(view().name("createProject"));
     }
 
-    /*@Test
+    @Test
     @DisplayName("Endpoint test for createProject")
     public void createProject() throws Exception {
-        ArrayList<String> reqTypeString = new ArrayList<>();
-        reqTypeString.add("1");
-        reqTypeString.add("2");
-
-        ProjectDom projectDom = new ProjectDom();
-        projectDom.setName("Project_Test");
-        projectDom.setType(ProjectType.AGILE);
-        projectDom.setDescription("projectTest");
-        projectDom.setStringReqTypes(reqTypeString);
-
         this.mockMvc.perform(post("/projects")
                                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                                .sessionAttr("project", projectDom)
+                                .param("name", "Project_Test")
+                                .param("type", "AGILE")
+                                .param("description", "projectTest")
+                                .param("stringReqTypes", "1,2")
                             )
                             .andExpect(status().isOk())
                             .andExpect(view().name("project"))
                             .andExpect(model().attribute("project",hasProperty("name",notNullValue())))
                             .andExpect(model().attribute("project",hasProperty("description")))
-                            .andExpect(model().attribute("project", hasProperty("projectType", notNullValue())));
-    }*/
+                            .andExpect(model().attribute("project", hasProperty("type", notNullValue())));
+    }
 
     @Test
     @DisplayName("Endpoint test for updateProject view")
@@ -79,6 +80,24 @@ public class ProjectControllerITest {
     }
 
     @Test
+    @DisplayName("Endpoint test for updateProject")
+    public void updateProject() throws Exception {
+        List<Project> projectList = projectRepository.findAll();
+        int size = projectList.size();
+        Project project = projectList.get(size-1);
+        int id = project.getIdProject();
+        this.mockMvc.perform(post("/project/"+String.valueOf(id))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("description", "projectTestUpdated")
+                .param("stringReqTypes", "3,4")
+        )
+                .andExpect(status().isOk())
+                .andExpect(view().name("project"))
+                .andExpect(model().attribute("project",hasProperty("description")))
+                .andExpect(model().attribute("project", hasProperty("stringReqTypes")));
+    }
+
+    @Test
     @DisplayName("Endpoint test for project view")
     public void getProjectView() throws Exception {
         this.mockMvc.perform(get("/project/8"))
@@ -86,5 +105,23 @@ public class ProjectControllerITest {
                 .andExpect(view().name("project"));
     }
 
-
+    @Test
+    @DisplayName("Endpoint test for delete project correct request")
+    public void deleteProject_ReturnsOk() throws Exception {
+        List<Project> projectList = projectRepository.findAll();
+        int size = projectList.size();
+        Project project = projectList.get(size-1);
+        int id = project.getIdProject();
+        this.mockMvc.perform(delete("/project/"+String.valueOf(id))).andExpect(status().isOk());
+    }
+    @Test
+    @DisplayName("Endpoint test for delete project error request")
+    public void deleteProject_ReturnsError() throws Exception {
+        List<Project> projectList = projectRepository.findAll();
+        int size = projectList.size();
+        Project project = projectList.get(size-1);
+        int id = project.getIdProject()+1;
+        this.mockMvc.perform(delete("/project/"+String.valueOf(id)))
+                .andExpect(status().isInternalServerError());
+    }
 }
