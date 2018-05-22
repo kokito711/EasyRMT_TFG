28c59635f1c9edbc83e9c6d7155a49788d272036
Thekoko <coco711@hotmail.es>
2018-05-22 19:54:17 +0200

    project service tests finished

--- EasyRMT/src/test/java/com/Sergio/EasyRMT/UnitTests/Service/ProjectServiceTest.java
+++ EasyRMT/src/test/java/com/Sergio/EasyRMT/UnitTests/Service/ProjectServiceTest.java
@@ -2,34 +2,35 @@ package com.Sergio.EasyRMT.UnitTests.Service;
 
 import com.Sergio.EasyRMT.Domain.ProjectDom;
 import com.Sergio.EasyRMT.Domain.RequirementTypeDom;
+import com.Sergio.EasyRMT.Model.Group;
 import com.Sergio.EasyRMT.Model.Project;
 import com.Sergio.EasyRMT.Model.RequirementType;
+import com.Sergio.EasyRMT.Model.types.ProjectType;
+import com.Sergio.EasyRMT.Repository.GroupRepository;
+import com.Sergio.EasyRMT.Repository.ObjectRepository;
 import com.Sergio.EasyRMT.Repository.ProjectRepository;
 import com.Sergio.EasyRMT.Repository.ReqTypeRepository;
 import com.Sergio.EasyRMT.Service.Converter.ProjectConverter;
 import com.Sergio.EasyRMT.Service.Converter.ReqTypeConverter;
-import com.Sergio.EasyRMT.Service.DocumentService;
-import com.Sergio.EasyRMT.Service.ProjectService;
+import com.Sergio.EasyRMT.Service.*;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.context.annotation.Description;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ProjectServiceTest {
-/*
+
     @Mock
     private ProjectRepository projectRepository;
     @Mock
@@ -40,6 +41,22 @@ public class ProjectServiceTest {
     private ReqTypeConverter reqTypeConverter;
     @Mock
     private DocumentService documentService;
+    @Mock
+    private GroupRepository groupRepository;
+    @Mock
+    private ObjectRepository objectRepository;
+    @Mock
+    private TraceabilityService traceabilityService;
+    @Mock
+    private FeatureService featureService;
+    @Mock
+    private EpicService epicService;
+    @Mock
+    private UseCaseService useCaseService;
+    @Mock
+    private UserStoryService userStoryService;
+    @Mock
+    private RequirementService requirementService;
 
     @BeforeEach
     public void initMocks(){
@@ -48,6 +65,14 @@ public class ProjectServiceTest {
         projectConverter = mock(ProjectConverter.class);
         reqTypeConverter = mock(ReqTypeConverter.class);
         documentService = mock(DocumentService.class);
+        groupRepository = mock(GroupRepository.class);
+        objectRepository = mock(ObjectRepository.class);
+        traceabilityService = mock(TraceabilityService.class);
+        featureService = mock(FeatureService.class);
+        epicService = mock(EpicService.class);
+        useCaseService = mock(UseCaseService.class);
+        userStoryService = mock(UserStoryService.class);
+        requirementService = mock(RequirementService.class);
     }
 
     @Test
@@ -55,17 +80,14 @@ public class ProjectServiceTest {
     public void getListProjects_ReturnsList(){
         List<Project> projectList = mock(List.class);
         List<ProjectDom> projectDomList = mock(List.class);
-        doReturn(projectList).when(projectRepository).findAll();
+        doReturn(projectList).when(projectRepository).findByGroup(1);
         doReturn(projectDomList).when(projectConverter).toDomain(projectList);
 
         ProjectService projectService = createProjectService();
 
         //Test conditions
-        assertTrue(projectService.getProjects().equals(projectDomList));
-        //Verify that method calls one to project repository
-        verify(projectRepository, times(1)).findAll();
-
-        //Verify that method calls one time to project converter
+        assertTrue(projectService.getProjects(1).equals(projectDomList));
+        verify(projectRepository, times(1)).findByGroup(1);
         verify(projectConverter, times(1)).toDomain(projectList);
     }
 
@@ -81,10 +103,7 @@ public class ProjectServiceTest {
 
         //Test conditions
         assertTrue( projectService.getProject(7).equals(projectDom));
-        //Verify that method calls one to project repository
         verify(projectRepository, times(1)).findByIdProject(anyInt());
-
-        //Verify that method calls one time to project converter
         verify(projectConverter, times(1)).toDomain(project);
     }
 
@@ -93,15 +112,14 @@ public class ProjectServiceTest {
     public void deleteProject_IdProjectProvided_DeletesProject(){
         doReturn(true).doReturn(false).when(projectRepository).existsByIdProject(anyInt());
         doNothing().when(projectRepository).deleteProjectByIdProject(anyInt());
+        doNothing().when(documentService).deleteFiles(7, null);
 
         ProjectService projectService = createProjectService();
 
         assertTrue(projectService.deleteProject(7));
-        //Verify that method calls two times to project repository.existsByIdProject
         verify(projectRepository, times(2)).existsByIdProject(anyInt());
-        //Verify that projectRepository.deleteProjectByIdProject() is called once
         verify(projectRepository, times(1)).deleteProjectByIdProject(anyInt());
-
+        verify(documentService, times(1)).deleteFiles(7, null);
     }
 
     @Test
@@ -111,12 +129,10 @@ public class ProjectServiceTest {
 
         ProjectService projectService = createProjectService();
 
-        //Test conditions
         assertFalse(projectService.deleteProject(7));
-        //Verify that method calls one time to project repository.existsByIdProject
         verify(projectRepository, times(1)).existsByIdProject(anyInt());
-        //Verify that projectRepository.deleteProjectByIdProject() is never called
         verify(projectRepository, times(0)).deleteProjectByIdProject(anyInt());
+        verify(documentService, times(0)).deleteFiles(7, null);
 
     }
 
@@ -128,22 +144,22 @@ public class ProjectServiceTest {
         ArrayList<String> reqTypesString = new ArrayList<>();
         reqTypesString.add("1");
         reqTypesString.add("2");
+        Group group = mock(Group.class);
 
         when(projectDom.getStringReqTypes()).thenReturn(reqTypesString);
+        when(projectDom.getGroupId()).thenReturn(1);
         when(projectConverter.toModel(projectDom)).thenReturn(project);
         when(projectRepository.save(project)).thenReturn(project);
         when(projectConverter.toDomain(project)).thenReturn(projectDom);
+        when(groupRepository.findOne(1)).thenReturn(group);
 
         ProjectService projectService = createProjectService();
 
-        //Test conditions
         assertTrue(projectService.createProject(projectDom).equals(projectDom));
-        //Verify that projectConverter.toModel has been called once
         verify(projectConverter, times(1)).toModel(projectDom);
-        //verify that projectRepository.save has been called once
         verify(projectRepository, times(1)).save(project);
-        //verify that projectConverter.toDomain has been called once
         verify(projectConverter, times(1)).toDomain(project);
+        verify(groupRepository, times(1)).findOne(1);
     }
 
     @Test
@@ -152,12 +168,11 @@ public class ProjectServiceTest {
         ProjectDom projectDom = mock(ProjectDom.class);
         Project project = mock(Project.class);
         ProjectDom projectParam =new ProjectDom();
-        ArrayList<RequirementType> reqTypes = mock(ArrayList.class);
-
+        List<RequirementType> reqTypes = mock(List.class);
+        Group group = mock(Group.class);
         ArrayList<String> reqTypesString = new ArrayList<>();
         reqTypesString.add("1");
         reqTypesString.add("2");
-
         projectParam.setDescription("description1");
         projectParam.setStringReqTypes(reqTypesString);
 
@@ -170,23 +185,23 @@ public class ProjectServiceTest {
         when(project.getDescription()).thenReturn("description");
         when(project.getRequirementTypes()).thenReturn(reqTypes);
         when(projectRepository.save(project)).thenReturn(project);
+        when(project.getGroup()).thenReturn(group);
+        when(group.getGroup_id()).thenReturn(1);
+        when(projectDom.getGroupId()).thenReturn(2);
+        when(groupRepository.findOne(1)).thenReturn(group);
 
         ProjectService projectService = createProjectService();
         //Test conditions
         assertTrue(projectService.updateProject(7,projectParam).equals(projectDom));
-        //verify projectRepository.findByIdProject has been called once
         verify(projectRepository,times(1)).findByIdProject(anyInt());
-        //verify projectRepository.save has been called once
         verify(projectRepository,times(1)).save(any(Project.class));
-        //verify projectConverter.toModel has been called once
         verify(projectConverter,times(1)).toModel(any(ProjectDom.class));
-        //verify projectConverter.toDomain has been called two times
         verify(projectConverter,times(2)).toDomain(any(Project.class));
 
     }
 
     @Test
-    @Description("Get Requirement types returns a list of requirement types")
+    @DisplayName("Get Requirement types returns a list of requirement types")
     public void getReqTypes_ReturnsList(){
         List<RequirementType> requirementTypeList = mock(List.class);
         List<RequirementTypeDom> requirementTypeDomList = mock(List.class);
@@ -197,13 +212,106 @@ public class ProjectServiceTest {
 
         //Test conditions
         assertTrue(projectService.getReqTypes().equals(requirementTypeDomList));
-        //Verify reqTypeRepositoru.findAll has been called once
         verify(reqTypeRepository, times(1)).findAll();
-        //Verify reTypeConverter.ToDomain has been called once
         verify(reqTypeConverter, times(1)).toDomain(requirementTypeList);
     }
 
+    @Test
+    @DisplayName("Get Requirement type returns a  requirement type")
+    public void getReqType_ReqTypeIdProvided_ReturnsRequirementType(){
+        RequirementType requirementType = mock(RequirementType.class);
+        RequirementTypeDom requirementTypeDom = mock(RequirementTypeDom.class);
+        when(reqTypeRepository.findByIdType(anyInt())).thenReturn(Optional.of(requirementType));
+        doReturn(requirementTypeDom).when(reqTypeConverter).toDomain(requirementType);
+
+        ProjectService projectService = createProjectService();
+
+        //Test conditions
+        assertTrue( projectService.getReqType(7).equals(requirementTypeDom));
+        verify(reqTypeRepository, times(1)).findByIdType(anyInt());
+        verify(reqTypeConverter, times(1)).toDomain(requirementType);
+    }
+
+    @Test
+    @DisplayName("Get Traced stats type returns a map with traceability stats")
+    public void getTracedStats_ProjectIdProvided_MapReturned(){
+        Integer tracedReqs = 1;
+        Integer notTracedReqs = 1;
+        when(projectRepository.getTracedObjects(1)).thenReturn(tracedReqs);
+        when(projectRepository.getNotTracedObjects(1)).thenReturn(notTracedReqs);
+
+        ProjectService projectService = createProjectService();
+        Map<String,Integer> expected = new HashMap<>();
+        expected.put("tracedReqs", tracedReqs);
+        expected.put("notTracedReqs", notTracedReqs);
+
+        //Test conditions
+        assertEquals(projectService.getTracedStats(1), expected);
+        verify(projectRepository, times(1)).getTracedObjects(1);
+        verify(projectRepository, times(1)).getNotTracedObjects(1);
+    }
+
+    @Test
+    @DisplayName("Get State stats type returns a map with Agile State stats")
+    public void getStateStats_AgileProjectIdProvided_MapReturned(){
+        List<Integer> requirementsList = mock(List.class);
+        List<Integer> objectLvl1Serie= mock(List.class);
+        List<Integer> objectLvl2Serie= mock(List.class);
+        Project project = mock(Project.class);
+        when(projectRepository.findByIdProject(1)).thenReturn(Optional.of(project));
+        when(project.getType()).thenReturn(ProjectType.AGILE);
+        when(projectRepository.getEpicsState(1)).thenReturn(objectLvl1Serie);
+        when(projectRepository.getUserStoriesState(1)).thenReturn(objectLvl2Serie);
+        when(projectRepository.getRequirementsState(1)).thenReturn(requirementsList);
+
+        ProjectService projectService = createProjectService();
+        Map<String,List> expected = new HashMap<>();
+        expected.put("objectLvl1Serie", objectLvl1Serie);
+        expected.put("objectLvl2Serie", objectLvl2Serie);
+        expected.put("requirements", requirementsList);
+
+        //Test conditions
+        assertEquals(projectService.getStateStats(1), expected);
+        verify(projectRepository, times(1)).findByIdProject(1);
+        verify(projectRepository, times(1)).getEpicsState(1);
+        verify(projectRepository, times(1)).getUserStoriesState(1);
+        verify(projectRepository, times(1)).getRequirementsState(1);
+        verify(projectRepository, times(0)).getFeaturesState(1);
+        verify(projectRepository, times(0)).getUseCasesState(1);
+    }
+
+    @Test
+    @DisplayName("Get State stats type returns a map with traditional State stats")
+    public void getStateStats_TraditionalProjectIdProvided_MapReturned(){
+        List<Integer> requirementsList = mock(List.class);
+        List<Integer> objectLvl1Serie= mock(List.class);
+        List<Integer> objectLvl2Serie= mock(List.class);
+        Project project = mock(Project.class);
+        when(projectRepository.findByIdProject(1)).thenReturn(Optional.of(project));
+        when(project.getType()).thenReturn(ProjectType.NOT_AGILE);
+        when(projectRepository.getFeaturesState(1)).thenReturn(objectLvl1Serie);
+        when(projectRepository.getUseCasesState(1)).thenReturn(objectLvl2Serie);
+        when(projectRepository.getRequirementsState(1)).thenReturn(requirementsList);
+
+        ProjectService projectService = createProjectService();
+        Map<String,List> expected = new HashMap<>();
+        expected.put("objectLvl1Serie", objectLvl1Serie);
+        expected.put("objectLvl2Serie", objectLvl2Serie);
+        expected.put("requirements", requirementsList);
+
+        //Test conditions
+        assertEquals(projectService.getStateStats(1), expected);
+        verify(projectRepository, times(1)).findByIdProject(1);
+        verify(projectRepository, times(0)).getEpicsState(1);
+        verify(projectRepository, times(0)).getUserStoriesState(1);
+        verify(projectRepository, times(1)).getRequirementsState(1);
+        verify(projectRepository, times(1)).getFeaturesState(1);
+        verify(projectRepository, times(1)).getUseCasesState(1);
+    }
+
     private ProjectService createProjectService(){
-        return new ProjectService(projectRepository, reqTypeRepository, projectConverter, reqTypeConverter, documentService);
-    }*/
+        return new ProjectService(projectRepository, reqTypeRepository, projectConverter, reqTypeConverter,
+                documentService, groupRepository, objectRepository, traceabilityService, featureService,
+                epicService, useCaseService, userStoryService, requirementService);
+    }
 }
